53d016ba26144c36abc2820de897f9460007ff1c
dima <>
2025-08-11 22:55:25 +0300

    начал делать секцию browse categories

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import CategoryCard from '../CategoryCard/CategoryCard';
+import styles from './Categories.module.css';
 
 function Categories({ data: categories = [] }) {
   return (
-    <section className="categories">
-      <div className="container">
+    <section className={styles.categories}>
+      <div className={styles.container}>
         <h2>Browse Categories</h2>
 
-        <ul className="cats">
+        <ul className={styles.cats}>
           {categories.map((categoryObject) => (
             <CategoryCard
               key={categoryObject.title}
--- src/components/CategoryCard/CategoryCard.jsx
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-
+import styles from "./CategoryCard.module.css";
 const basePath = "./images/";
 
 function CategoryCard({ category }) {
   return (
-    <li className="cat">
-      <img src={basePath + category.img} alt={category.title} />
+    <li className={styles.cat}>
+      <img src={basePath + category.img} alt={category.title} className={styles.img} />
       <i>
-        <img src={basePath + category.icon} alt="" />
+        <img src={basePath + category.icon} alt="" className={styles.icon} />
       </i>
       <h4>{category.title}</h4>
     </li>
