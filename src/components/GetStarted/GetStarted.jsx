c3988ae3c73ba2c4357270b7bee8e99aa843be28
dima <>
2025-08-13 11:25:47 +0300

    сверстал секцию getstarted

--- src/components/GetStarted/GetStarted.jsx
+++ src/components/GetStarted/GetStarted.jsx
@@ -1,27 +1,28 @@
 import React from "react";
+import styles from "./GetStarted.module.css";
 
 function GetStarted() {
   return (
-    <section className="get-started">
-      <div className="container">
+    <section className={styles.getstarted}>
+      <div className={styles.container}>
         <h2>How it works</h2>
         <p>Find out how to get started</p>
 
-        <ul className="steps">
-          <li className="step">
-            <img src="" alt=""/>
-              <h4>Setup Your wallet</h4>
-              <p>Set up your wallet of choice. Connect it to the Animarket by clicking the wallet icon in the top right corner.</p>
+        <ul className={styles.steps}>
+          <li className={styles.step}>
+            <img src="/images/icons/wallet.svg" alt="wallet" />
+            <h4>Setup Your wallet</h4>
+            <p>Set up your wallet of choice. Connect it to the Animarket by clicking the wallet icon in the top right corner.</p>
           </li>
-          <li className="step">
-            <img src="" alt=""/>
-              <h4>Create Collection</h4>
-              <p>Upload your work and setup your collection. Add a description, social links and floor price.</p>
+          <li className={styles.step}>
+            <img src="/images/icons/collection.svg" alt="collection" />
+            <h4>Create Collection</h4>
+            <p>Upload your work and setup your collection. Add a description, social links and floor price.</p>
           </li>
-          <li className="step">
-            <img src="" alt=""/>
-              <h4>Start Earning</h4>
-              <p>Choose between auctions and fixed-price listings. Start earning by selling your NFTs or trading others.</p>
+          <li className={styles.step}>
+            <img src="/images/icons/basket.svg" alt="basket" />
+            <h4>Start Earning</h4>
+            <p>Choose between auctions and fixed-price listings. Start earning by selling your NFTs or trading others.</p>
           </li>
         </ul>
       </div>
