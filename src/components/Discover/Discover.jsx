d3e42e5a0ceb83fd1e51fe6c9cb55782f0e6fbbf
dima <>
2025-08-22 21:44:15 +0300

    добавил адаптив для секции Discover, улучшил отображение на мобильных устройствах

--- src/components/Discover/Discover.jsx
+++ src/components/Discover/Discover.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 import NftCard from "../NftCard/NftCard"
-
 import styles from "./Discover.module.css";
+import useWindowWidth from "../../hooks/useWindowWidth";
 
 function Discover({ data: nfts = [] }) {
+  const width = useWindowWidth();
+
+  let itemsToShow;
+  if (width < 1280) { // Для планшета и мобильного
+    itemsToShow = 2;
+  } else { // Для десктопа
+    itemsToShow = 3;
+  }
+
+  if (width < 834) {
+    itemsToShow = 3;
+  }
+
+  const visibleNfts = nfts.slice(0, itemsToShow);
+
   return (
     <section className={styles.discover}>
       <div className={styles.container}>
@@ -12,19 +27,23 @@ function Discover({ data: nfts = [] }) {
             <h2>Discover More NFTs</h2>
             <p>Explore new trending NFTs</p>
           </hgroup>
-          <button>
+          <button className={styles.desktopButton}>
             <img src="/images/icons/eye.svg" alt="eye" />
             See All
-            </button>
+          </button>
         </header>
         <ul className={styles.nfts}>
-        {nfts.map((nftObject) => (
-          <NftCard
-          key={nftObject.title}
-          nft={nftObject} 
-        />
-      ))}
+          {visibleNfts.map((nftObject) => (
+            <NftCard
+              key={nftObject.title}
+              nft={nftObject}
+            />
+          ))}
         </ul>
+        <button className={styles.mobileButton}>
+          <img src="/images/icons/eye.svg" alt="eye" />
+          See All
+        </button>
       </div>
     </section>
   )
