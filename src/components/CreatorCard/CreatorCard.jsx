01f7c3125694b3ee02618a116d5223072a82e6b7
dima <>
2025-08-22 19:25:13 +0300

    добавил адаптив для секции TopCreators и CreatorCard, улучшил отображение на мобильных устройствах

--- src/components/CreatorCard/CreatorCard.jsx
+++ src/components/CreatorCard/CreatorCard.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import styles from "./CreatorCard.module.css";
 const basePath = "./images/"
 
-function CreatorCard({creator}) {
+function CreatorCard({ creator }) {
   return (
     <li className={styles.creator}>
-        <i className={styles.position}>{creator.position}</i>
-        <img src={basePath + creator.ava} alt={creator.name} className={styles.ava} />
+      <i className={styles.position}>{creator.position}</i>
+      <img src={basePath + creator.ava} alt={creator.name} className={styles.ava} />
+      <div className={styles.info}>
         <h4 className={styles.name}>{creator.name}</h4>
         <div className={styles.row}>
           <span>Total Sales:</span>
           <span className={styles.amount}>{creator.amount}</span>
         </div>
-      </li>
+      </div>
+    </li>
   )
 }
 
--- src/components/TopCreators/TopCreators.jsx
+++ src/components/TopCreators/TopCreators.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import styles from "./TopCreators.module.css";
-
 import CreatorCard from "../CreatorCard/CreatorCard";
+import useWindowWidth from "../../hooks/useWindowWidth";
 
 function TopCreators({ data: creators = [] }) {
+  const width = useWindowWidth();
+
+  // Решаем, сколько карточек показывать
+  let itemsToShow;
+  if (width < 834) { // Мобильный
+    itemsToShow = 5;
+  } else if (width < 1280) { // Планшет
+    itemsToShow = 6;
+  } else { // Десктоп
+    itemsToShow = 12;
+  }
+
+  const visibleCreators = creators.slice(0, itemsToShow);
+
   return (
     <section className={styles.topcreators}>
       <div className={styles.container}>
@@ -12,14 +26,28 @@ function TopCreators({ data: creators = [] }) {
             <h2>Top creators</h2>
             <p>Checkout Top Rated Creators on the NFT Marketplace</p>
           </hgroup>
-          <button>
-            <img src="/images/icons/rocket-purple.png" alt="purple-rocket" />
+          <button className={styles.desktopButton}>
+            <img src="/images/icons/rocket-purple.svg" alt="purple-rocket" />
             View Rankings
           </button>
         </header>
 
-        <ul className={styles.creators}>
-          {creators.map((creatorObject) => (
+        <ul className={styles.mobileCreators}>
+          {visibleCreators.map((creatorObject) => (
+            <CreatorCard
+              key={creatorObject.position}
+              creator={creatorObject}
+            />
+          ))}
+        </ul>
+
+        <button className={styles.mobileButton}>
+          <img src="/images/icons/rocket-purple.svg" alt="purple-rocket" />
+          View Rankings
+        </button>
+
+        <ul className={styles.desktopCreators}>
+          {visibleCreators.map((creatorObject) => (
             <CreatorCard
               key={creatorObject.position}
               creator={creatorObject}
