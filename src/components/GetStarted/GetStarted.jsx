11f8d7c4d9e00f306a4b2723b7efdedc1f7d8c8c
dima <>
2025-08-23 12:48:48 +0300

    добавил адаптив для секции GetStarted, улучшил отображение на мобильных устройствах

--- src/components/GetStarted/GetStarted.jsx
+++ src/components/GetStarted/GetStarted.jsx
@@ -11,18 +11,24 @@ function GetStarted() {
         <ul className={styles.steps}>
           <li className={styles.step}>
             <img src="/images/icons/wallet.svg" alt="wallet" />
-            <h4>Setup Your wallet</h4>
-            <p>Set up your wallet of choice. Connect it to the Animarket by clicking the wallet icon in the top right corner.</p>
+            <div className={styles.info}>
+              <h4>Setup Your wallet</h4>
+              <p>Set up your wallet of choice. Connect it to the Animarket by clicking the wallet icon in the top right corner.</p>
+            </div>
           </li>
           <li className={styles.step}>
             <img src="/images/icons/collection.svg" alt="collection" />
-            <h4>Create Collection</h4>
-            <p>Upload your work and setup your collection. Add a description, social links and floor price.</p>
+            <div className={styles.info}>
+              <h4>Create Collection</h4>
+              <p>Upload your work and setup your collection. Add a description, social links and floor price.</p>
+            </div>
           </li>
           <li className={styles.step}>
             <img src="/images/icons/basket.svg" alt="basket" />
-            <h4>Start Earning</h4>
-            <p>Choose between auctions and fixed-price listings. Start earning by selling your NFTs or trading others.</p>
+            <div className={styles.info}>
+              <h4>Start Earning</h4>
+              <p>Choose between auctions and fixed-price listings. Start earning by selling your NFTs or trading others.</p>
+            </div>
           </li>
         </ul>
       </div>
